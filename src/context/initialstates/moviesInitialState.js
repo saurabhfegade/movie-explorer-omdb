7e172c87f4c1e02c864f5f13b44b34a06a35f541
saurabhfegade <saurabh.fegade1998@gmail.com>
2021-04-22 18:09:13 +0530

    reducer update

--- src/context/initialstates/moviesInitialState.js
+++ src/context/initialstates/moviesInitialState.js
@@ -1,15 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  // movies: {
   loading: false,
   error: null,
   data: [],
   carouselMovies: [],
-  // isSearchActive: false,
   foundMovies: [],
   foundMovie: [],
   totalMovies: 0,
-  page: 1,
-  hasNextPage: true,
-  // },
 };
--- src/context/reducers/movies.js
+++ src/context/reducers/movies.js
@@ -15,84 +15,54 @@ const movies = (state = moviesInitialState, { payload, type }) => {
     case MOVIES_LOADING: {
       return {
         ...state,
-        // movies: {
-        //   ...state.movies,
         loading: true,
-        // },
       };
     }
     case MOVIES_LOAD_SUCCESS: {
       return {
         ...state,
-        // movies: {
-        //   ...state.movies,
         loading: false,
         data: [...state.data, payload],
-        // data: state.data.concat(payload),
-        // foundMovies: [],
-        // },
       };
     }
     case CAROUSEL_MOVIES_LOAD_SUCCESS: {
       return {
         ...state,
-        // movies: {
-        //   ...state.movies,
         loading: false,
         carouselMovies: [...state.carouselMovies, payload],
-        // data: state.data.concat(payload),
-        // foundMovies: [],
-        // },
       };
     }
     case MOVIE_LOAD_SUCCESS: {
       return {
         ...state,
-        // movies: {
-        //   ...state.movies,
         loading: false,
-        // data: [...state.data, payload],
         foundMovie: payload,
         carouselMovies: [],
-        // },
       };
     }
     case MOVIES_SEARCH_SUCCESS: {
       return {
         ...state,
-        // movies: {
-        //   ...state.movies,
         loading: false,
-        // data: [...state.data, payload],
         foundMovies: state.foundMovies.concat(
           payload.Search.map((movie) => movie.imdbID)
         ),
-        // data: [],
-        // totalMovies: payload.totalResults,
-        // },
       };
     }
     case MOVIES_SEARCH_BUTTON_SUCCESS: {
       return {
         ...state,
-        // movies: {
-        //   ...state.movies,
         loading: false,
-        // data: [...state.data, payload],
         foundMovies: payload.Search.map((movie) => movie.imdbID),
         data: [],
         totalMovies: payload.totalResults,
-        // },
       };
     }
     case MOVIES_LOAD_ERROR: {
       return {
         ...state,
-        // movies: {
-        //   ...state.movies,
         loading: false,
         error: payload,
-        // },
       };
     }
     default:
